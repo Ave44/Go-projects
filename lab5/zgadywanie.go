e515a351e8185c40700103f61e6e77f4f34c06f2
Paweł Olszewski <monte.paw@gmail.com>
2022-04-04 22:47:41 +0200

    Poziom 2 i 3

--- lab5/zgadywanie.go
+++ lab5/zgadywanie.go
@@ -3,41 +3,73 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 	"strconv"
+	"time"
 )
 
-func getUserNumber(komunikat string) int {
+func getUserNumber(komunikat string) (int, bool) {
 	liczbaString := ""
 	fmt.Print(komunikat)
 	fmt.Scan(&liczbaString)
 
+	if liczbaString == "koniec" {
+		return -1, false
+	}
+
 	liczba, err := strconv.Atoi(liczbaString)
 	if err == nil && liczba >= 0 && liczba <= 1000 {
-		return liczba
+		return liczba, true
 	}
 	return getUserNumber("Należy podać liczbę od 0 do 1000: ")
 }
 
-func main() {
-
-	liczba := -1
-	max := 1000
-	min := 0
-
+func getRandomNumber(min int, max int) int {
 	rand.Seed(time.Now().UnixNano())
-	loswa := rand.Intn(max - min) + min
+	return rand.Intn(max-min) + min
+}
 
-	liczba = getUserNumber("Podaj liczbę od 0 do 1000: ")
+func victory() bool {
+	anwser := ""
+	fmt.Print("Gratulacje zgadłeś!\n")
+	for anwser != "T" && anwser != "N" {
+		fmt.Print("Czy chcesz kontynuować? [T/N]: ")
+		fmt.Scan(&anwser)
+	}
+	if anwser == "T" {
+		return true
+	}
+	return false
+}
 
-	for liczba != loswa {
-		if liczba < loswa {
-			liczba = getUserNumber("Podano za małą liczbę: ")
-		} else {
-			liczba = getUserNumber("Podano za dużą liczbę: ")
+func game() {
+	play := true
+
+MainLoop:
+	for play {
+		randNum := getRandomNumber(0, 5)
+		liczba, continueGame := getUserNumber("Podaj liczbę od 0 do 1000: ")
+		if !continueGame {
+			break MainLoop
 		}
+
+		for liczba != randNum {
+			if liczba < randNum {
+				liczba, continueGame = getUserNumber("Podano za małą liczbę: ")
+			} else {
+				liczba, continueGame = getUserNumber("Podano za dużą liczbę: ")
+			}
+			if !continueGame {
+				break MainLoop
+			}
+		}
+
+		play = victory()
 	}
 
-	fmt.Print("Gratulacje zgadłeś!")
+	fmt.Print("Dziękuję za grę!")
+}
 
+func main() {
+
+	game()
 }
